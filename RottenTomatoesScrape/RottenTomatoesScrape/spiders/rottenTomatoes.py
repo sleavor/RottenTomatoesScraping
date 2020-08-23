222bdb61d3b921093fbe7c2b49ec2ec069186db8
sleavor <sleavor@yahoo.com>
2020-08-23 17:06:53 -0400

    Errors fixed to grab all movies

--- RottenTomatoesScrape/RottenTomatoesScrape/spiders/rottenTomatoes.py
+++ RottenTomatoesScrape/RottenTomatoesScrape/spiders/rottenTomatoes.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from RottenTomatoesScrape.items import RottentomatoesscrapeItem
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
 import w3lib
 
-class RottentomatoesSpider(scrapy.Spider):
+class RottentomatoesSpider(SitemapSpider):
     name = 'rottenTomatoes'
     allowed_domains = ['www.rottentomatoes.com']
-    start_urls = ['https://www.rottentomatoes.com/m/kicking_and_screaming',
-                'https://www.rottentomatoes.com/m/star_wars_episode_iii_revenge_of_the_sith'
+    sitemap_urls=[]
+    for i in range(23):
+        sitemap_urls.append('https://www.rottentomatoes.com/sitemap_' + str(i) + '.xml')
+    
+    rules = [
+        ('/pictures/', ''),
+        ('/trailers/', ''),
+        ('/m/', 'parse'),
     ]
-
+    
     def parse(self, response):
+        if '/pictures' in response.url or '/trailers' in response.url:
+            if '/m/pictures' not in response.url or '/m/trailers' not in response.url:
+                return
         item = RottentomatoesscrapeItem()
         a = response.xpath('//div[@class="meta-value"]').extract()
         b = response.xpath('//div[@class="meta-label subtle"]').extract()
@@ -19,7 +28,7 @@ class RottentomatoesSpider(scrapy.Spider):
             if "Genre:" in b[c]:
                 genreList = []
                 genres = w3lib.html.remove_tags(a[c]).replace("\n","").strip()
-                for genre in genres.split(",",1):
+                for genre in genres.split(","):
                     genreList.append(genre.strip())
                 item['genre'] = genreList
             elif "Rating:" in b[c]:
@@ -29,7 +38,7 @@ class RottentomatoesSpider(scrapy.Spider):
             elif "Written By:" in b[c]:
                 writerList = []
                 writers = w3lib.html.remove_tags(a[c]).replace("\n","").strip()
-                for writer in writers.split(",",1):
+                for writer in writers.split(","):
                     writerList.append(writer.strip())
                 item['writer'] = writerList
             elif "In Theaters:" in b[c]:
@@ -40,10 +49,15 @@ class RottentomatoesSpider(scrapy.Spider):
                 item['runtime'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip()
             elif "Studio:" in b[c]:
                 item['studio'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip()
-        item['criticRate'] = int(response.xpath('//*[@id="tomato_meter_link"]/span[2]/text()').extract()[0].replace("\n", "").replace("%",""))
-        item['numCritReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[1]/div/small/text()').extract()[0].replace("\n", "").replace(",",""))
-        item['audienceRate'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()').extract()[0].replace("\n", "").replace("%",""))
-        item['numAudienceReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/div/strong/text()').extract()[0].replace("User Ratings: ", "").replace(",",""))
-        item['rateDiff'] = item['criticRate'] - item['audienceRate']
+        criticRate = response.xpath('//*[@id="tomato_meter_link"]/span[2]/text()').extract()
+        audienceRate = response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()').extract()
+        if criticRate:
+            item['criticRate'] = int(criticRate[0].replace("\n", "").replace("%",""))
+            item['numCritReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[1]/div/small/text()').extract()[0].replace("\n", "").replace(",",""))
+        if audienceRate:
+            item['audienceRate'] = int(audienceRate[0].replace("\n", "").replace("%",""))
+            item['numAudienceReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/div/strong/text()').extract()[0].replace("User Ratings: ", "").replace(",",""))
+        if audienceRate and criticRate:
+            item['rateDiff'] = item['criticRate'] - item['audienceRate']
         item['title'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/h1/text()').extract()[0]
         yield item
