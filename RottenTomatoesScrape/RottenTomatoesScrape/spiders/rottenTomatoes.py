22b08824f3d99acaa36376dd9a9f69df7fe164a2
sleavor <sleavor@yahoo.com>
2020-08-23 14:53:30 -0400

    Fixed issues with scraping

--- RottenTomatoesScrape/RottenTomatoesScrape/spiders/rottenTomatoes.py
+++ RottenTomatoesScrape/RottenTomatoesScrape/spiders/rottenTomatoes.py
@@ -2,6 +2,7 @@
 import scrapy
 from RottenTomatoesScrape.items import RottentomatoesscrapeItem
 from scrapy.spiders import CrawlSpider, Rule
+import w3lib
 
 class RottentomatoesSpider(scrapy.Spider):
     name = 'rottenTomatoes'
@@ -16,25 +17,33 @@ class RottentomatoesSpider(scrapy.Spider):
         b = response.xpath('//div[@class="meta-label subtle"]').extract()
         for c in range(len(b)):
             if "Genre:" in b[c]:
-                item['genre'] = a[c]
+                genreList = []
+                genres = w3lib.html.remove_tags(a[c]).replace("\n","").strip()
+                for genre in genres.split(",",1):
+                    genreList.append(genre.strip())
+                item['genre'] = genreList
             elif "Rating:" in b[c]:
-                item['rating'] = a[c]
+                item['rating'] = w3lib.html.remove_tags(a[c]).replace("\n", "").split(" ",1)[0].strip()
             elif "Directed By:" in b[c]:
-                item['director'] = a[c]
+                item['director'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip()
             elif "Written By:" in b[c]:
-                item['writer'] = a[c]
+                writerList = []
+                writers = w3lib.html.remove_tags(a[c]).replace("\n","").strip()
+                for writer in writers.split(",",1):
+                    writerList.append(writer.strip())
+                item['writer'] = writerList
             elif "In Theaters:" in b[c]:
-                item['airDate'] = a[c]
+                item['airDate'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip().split("  ",1)[0].strip()
             elif "Box Office:" in b[c]:
-                item['boxOffice'] = a[c]
+                item['boxOffice'] = int(w3lib.html.remove_tags(a[c]).replace("\n", "").replace("$","").replace(",",""))
             elif "Runtime:" in b[c]:
-                item['runtime'] = a[c]
+                item['runtime'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip()
             elif "Studio:" in b[c]:
-                item['studio'] = a[c]
-        item['criticRate'] = response.xpath('//*[@id="tomato_meter_link"]/span[2]/text()').extract()
-        item['numCritReviews'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[1]/div/small/text()').extract()
-        item['audienceRate'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()').extract()
-        item['numAudienceReviews'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/div/strong/text()').extract()
-        #item['rateDiff'] = item['criticRate'] - item['audienceRate']
-        item['title'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/h1/text()').extract()
+                item['studio'] = w3lib.html.remove_tags(a[c]).replace("\n", "").strip()
+        item['criticRate'] = int(response.xpath('//*[@id="tomato_meter_link"]/span[2]/text()').extract()[0].replace("\n", "").replace("%",""))
+        item['numCritReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[1]/div/small/text()').extract()[0].replace("\n", "").replace(",",""))
+        item['audienceRate'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()').extract()[0].replace("\n", "").replace("%",""))
+        item['numAudienceReviews'] = int(response.xpath('//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/div/strong/text()').extract()[0].replace("User Ratings: ", "").replace(",",""))
+        item['rateDiff'] = item['criticRate'] - item['audienceRate']
+        item['title'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/h1/text()').extract()[0]
         yield item
